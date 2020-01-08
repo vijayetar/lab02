99a73f8c9cc5ffe62220b2938f5747da5dd64633
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-08 10:33:59 -0800

    rendered images for feature 1

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # lab02
 
 Vij and Andrew did pair programming.
+
+Number and name of feature: __________Display Images- Feature One______________________
+
+Estimate of time needed to complete: _30min____
+
+Start time: __0940___
+
+Finish time: _____
+
+Actual time needed to complete: _____
+Add this information to your README.
--- js/app.js
+++ js/app.js
@@ -17,8 +17,17 @@ function Animal (Obj) {
 $.ajax('./data/page-1.json',{method:'GET', dataType:'JSON'})
   .then(data=> {
     data.forEach(value => {
-      new Animal(value);
+      new Animal(value).render();
     })
   });
 
-console.log('animals',animals);
+// console.log('animals',animals);
+
+// render images
+Animal.prototype.render = function () {
+  const imageTemplate = $('#image_template').html();
+  const $newSection = $('<section></section>');
+  $newSection.html(imageTemplate);
+  $newSection.find('img').attr('src',this.image_url);
+  $('main').append($newSection);
+};
