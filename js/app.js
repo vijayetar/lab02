6349b28ce8b3d68dfb9e8d11ec047bec1288680e
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-08 10:17:11 -0800

    completed constructor

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 'use strict';
 console.log("hello world");
+
+//constructor function to make new array
+
+const animals = [];
+function Animal (Obj) {
+  this.image_url = Obj.image_url;
+  this.title = Obj.title;
+  this.description = Obj.description;
+  this.keyword = Obj.keyword;
+  this.horns = Obj.horns;
+  animals.push(this);
+}
+
+//collecting the data from json file
+$.ajax('./data/page-1.json',{method:'GET', dataType:'JSON'})
+  .then(data=> {
+    data.forEach(value => {
+      new Animal(value);
+    })
+  });
+
+console.log('animals',animals);
