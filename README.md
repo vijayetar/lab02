888d5387c2880bfd9fec7258219074d56de1949f
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-08 12:06:50 -0800

    completed the keyword array

--- README.md
+++ README.md
@@ -8,7 +8,27 @@ Estimate of time needed to complete: _30min____
 
 Start time: __0940___
 
+Finish time: _1039____
+
+Actual time needed to complete: __40min___
+Add this information to your README.
+
+Number and name of feature: _____Feature 2: filter images___________________________
+
+Estimate of time needed to complete: __60min___
+
+Start time: __10:40___
+
 Finish time: _____
 
 Actual time needed to complete: _____
-Add this information to your README.
+
+Number and name of feature: ________________________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 'use strict';
-console.log("hello world");
+// console.log("hello world");
 
 //constructor function to make new array
-
 const animals = [];
 function Animal (Obj) {
   this.image_url = Obj.image_url;
@@ -10,6 +9,7 @@ function Animal (Obj) {
   this.description = Obj.description;
   this.keyword = Obj.keyword;
   this.horns = Obj.horns;
+  // keyArr.push(Obj.keyword);
   animals.push(this);
 }
 
@@ -17,11 +17,12 @@ function Animal (Obj) {
 $.ajax('./data/page-1.json',{method:'GET', dataType:'JSON'})
   .then(data=> {
     data.forEach(value => {
-      new Animal(value).render();
+      new Animal(value);
+      // console.log(value.keyword);
     })
+    dropOptions();
   });
 
-// console.log('animals',animals);
 
 // render images
 Animal.prototype.render = function () {
@@ -31,3 +32,45 @@ Animal.prototype.render = function () {
   $newSection.find('img').attr('src',this.image_url);
   $('main').append($newSection);
 };
+
+//make new array of keywords while making sure that they are unique
+const keyArr = [];
+//make a new ARRay without repeats
+const dropOptions = () => {
+  console.log(animals);
+  animals.forEach(aniObj => {
+    // console.log('hello');
+    if (!keyArr.includes(aniObj.keyword)) {
+      keyArr.push(aniObj.keyword);
+    }
+  })
+  dropDownMenu(keyArr);
+}
+
+
+
+
+function dropDownMenu() {
+  const $newDropDown = $('#dropdown').html();
+  ke
+  const $options = $('<option></option>');
+  $options.text(this.keyword);
+  // $options.html($newDropDown);
+  console.log('keyword',this.keyword);
+  $newDropDown.append($options);
+};
+
+
+//Feature 2
+// append the keywords to the dropdown
+// function keywords() {
+//   const $newDropDown = $('#dropdown').html();
+//   const $options = $('<option></option>');
+//   $newDropDown.html($options);
+// $options.html(animals[indx].)
+// };
+
+//make a dropdown menu with the keywords
+//make keywords an event handler
+// with each click, relevant images should be rendered
+//remove old images when next keyword is clicked
